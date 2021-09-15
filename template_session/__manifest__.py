4e4372fddd6cab49c63dafe04ec7443d4cee287b
Shahil <shahil@icreativetechnolabs.com>
2021-09-15 18:25:15 +0530

    [New] Controller

---
+++ template_session/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Template Session",
+    'summary': """
+        Demo module for template session""",
+    'author': "Shahil Chauhan",
+    'version': '14.0.1.0.0',    
+    'depends': ['website'],
+    'data': [
+        'views/demo_template.xml',
+        'views/assets.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+
+}
