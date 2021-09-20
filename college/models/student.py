9fc28a106686918eea30e2f7e9b751c47d88085c
Shahil <shahil@icreativetechnolabs.com>
2021-09-20 18:20:17 +0530

    [ADD] ORM Methods

--- college/models/student.py
+++ college/models/student.py
@@ -37,14 +37,52 @@ class Student(models.Model):
     student_fees = fields.Monetary(string="School Fees")
     active = fields.Boolean(string="Active")
 
+    # Create method to create new record
     @api.model 
     def create(self, values):
         print("\n\n\nvalues----",values)
         print("\n\n\nself----",self)
-        values['active'] = True
+        values['active'] = True #to set the value of active field while overriding create method.
         rtn = super(Student, self).create(values)
         print("\n\n\nreturned----",rtn)
         return rtn
+
+    # write method to update an exist record
+    # NO Decorator  
+    def write(self, vals):
+        print("\n\n\nself----", self)
+        print("\n\nValues-----",vals)
+        # vals['active'] = True #to update the value of active field while overriding write method.
+        rtn =  super(Student, self).write(vals)
+        print("\n\nReturn-----",rtn)
+        return rtn
+
+    # copy method to create new record from existing record
+    def copy(self, default=None):
+        print("\n\n\nself----", self)
+        # default['active'] = False # copy with active field false
+        rtn = super(Student, self).copy(default=default)
+        print("\n\nReturn-----", rtn)
+        return rtn
+
+    # unlink method to delete a record
+    def unlink(self):
+        print("\n\n\nself----", self)
+        # you can write your condition for unlink
+        rtn = super(Student, self).unlink()
+        print("\n\nReturn-----", rtn)
+        return rtn
+
+    # name_create method to create a record with name field.
+    def name_create(self, name):
+        # rtn = self.create({'name':name})
+        # return rtn.name_get()[0]
+        print("\n\nself--- ",self)
+        print("\n\n\nself----", name)
+        rtn = super(Student, self).name_create(name)
+        print("\n\nReturn-----", rtn)
+        return rtn
+
 
 
 class Hobby(models.Model):
