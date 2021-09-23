9e1e604456569a9598f82cb7462c5687535bc956
Shahil <shahil@icreativetechnolabs.com>
2021-09-23 12:08:10 +0530

    [Modify] template session

--- template_session/__manifest__.py
+++ template_session/__manifest__.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Template Session",
-    'summary': """
+    "name": "Template Session",
+    "summary": """
         Demo module for template session""",
-    'author': "Shahil Chauhan",
-    'version': '14.0.1.0.0',    
-    'depends': ['website'],
-    'data': [
-        'views/demo_template.xml',
-        'views/assets.xml',
-        'data/menu_demo.xml',
+    "author": "Shahil Chauhan",
+    "version": "14.0.1.0.0",
+    "depends": ["website"],
+    "data": [
+        "views/demo_template.xml",
+        "views/assets.xml",
+        "data/menu_demo.xml",
     ],
-    'auto_install': False,
-    'installable': True,
-    'application': True,
-
+    "auto_install": False,
+    "installable": True,
+    "application": True,
 }
