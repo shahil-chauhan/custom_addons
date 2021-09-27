4f1a7edda88e47dfc1e5531aec73b67de883e1dd
Shahil <shahil@icreativetechnolabs.com>
2021-09-27 18:24:04 +0530

    [ADD] Special commands

---
+++ shahil_special_commands/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Special Commands",
+    "version": "14.0.1.0.0",
+    "website": "https://www.youtube.com",
+    "author": "Shahil Chauhan",
+    "category": "Testing, Learning",
+    "summary": """This module will demonstrate all special commands""",
+    "description": "This module provides total analyses of your all orders",
+    "depends": ["sale_management"],
+    "data": [
+        "view/sale_order_button.xml",
+    ],
+    "demo": [],
+    "sequence": 2,
+    "application": True,
+    "installable": True,
+    "auto-install": False,
+}
---
+++ shahil_special_commands/models/sale_order_button.py
@@ -0,0 +1,39 @@
+from odoo import fields, models
+from odoo.tools.translate import _
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    custom_partner_ids = fields.Many2many(
+        "res.partner", "custom_partner_rel", "parnter_id", "custom_id", string=_("Custom Partner Ids")
+    )
+
+    def update_m2m_button(self):
+        print("\n\n Update m2m button called")
+
+        """ (0,0,{}) -->  CREATE NEW RECORD IN DATABASE"""
+        range_list = []
+        for i in range(0, 2):
+            cust_dict = {'name': i}
+            range_list.append(cust_dict)
+        for val in range_list:
+            self.custom_partner_ids = [(0, 0, val)]
+
+        """ (1,id,{}) -->  UPDATE RECORD IN DATABASE IF EXIST """
+        # self.custom_partner_ids = [(1, 14, {'phone': '654987312'})]
+
+        """ (2,id) -->  REMOVE RECORD FROM DATABASE """
+        # self.custom_partner_ids= [(2,87)]
+
+        """ (3,id) -->  REMOVE RECORD FROM FIELD BUT NOT PERMENENT """
+        # self.custom_partner_ids= [(3,88)]
+
+        """ (4,id) -->  ADD EXISTING RECORD IN DATABASE"""
+        # self.custom_partner_ids = [(4,27)]
+
+        """ (5,) -->  UNLINK ALL RECORD FROM FIELD BUT NOT PERMENENT """
+        # self.custom_partner_ids = [(5,)]
+
+        """ (6,0,[IDs]) -->  REMOVE RECORD FROM DATABASE """
+        # self.custom_partner_ids = [(6, 0, [14, 23, 33])]
