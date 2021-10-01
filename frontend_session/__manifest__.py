185c6152ae369e16a5a6353718bdc87490dc5cc1
Shahil <shahil@icreativetechnolabs.com>
2021-10-01 10:14:48 +0530

    [ADD] Frontend

--- frontend_session/__manifest__.py
+++ frontend_session/__manifest__.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Frontend Session",
-    'summary': """
+    "name": "Frontend Session",
+    "summary": """
         Demo module for template session""",
-    'author': "Shahil Chauhan",
-    'version': '14.0.1.0.0',    
-    'depends': ['website_sale'],
-    'license': "AGPL-3",
-    'data': [
-        'data/website_menu.xml',
-        'views/assets.xml',
-        'views/contacts_list_template.xml',
+    "author": "Shahil Chauhan",
+    "version": "14.0.1.0.0",
+    "depends": ["website_sale"],
+    "license": "AGPL-3",
+    "data": [
+        "data/website_menu.xml",
+        "views/assets.xml",
+        "views/contacts_list_template.xml",
     ],
-    'auto_install': False,
-    'installable': True,
-    'application': True,
-
+    "auto_install": False,
+    "installable": True,
+    "application": True,
 }
--- frontend_session/controllers/controllers.py
+++ frontend_session/controllers/controllers.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 import json
+
 from odoo import http
-from odoo.http import request, route
 from odoo.addons.website.controllers.main import Website
+from odoo.http import request, route
+
 
 class Controller(http.Controller):
     @http.route("/contacts", type="http", website=True, auth="public")
     def demo_page(self):
-        contacts = request.env['res.partner'].sudo().search([], order='name asc')
-        return request.render("frontend_session.contacts_list", {'contacts': contacts})
+        contacts = request.env["res.partner"].sudo().search([], order="name asc")
+        return request.render("frontend_session.contacts_list", {"contacts": contacts})
+
 
 class portal(Website):
     @http.route()
