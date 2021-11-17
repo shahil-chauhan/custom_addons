ade466b7cf1d66d9a4b105a803d9e99eccdaf154
Shahil <shahil@icreativetechnolabs.com>
2021-11-17 16:42:32 +0530

    [ADDED] Pager, Sort and filter

--- construction_site/__manifest__.py
+++ construction_site/__manifest__.py
@@ -9,18 +9,19 @@
     "version": "14.0.1.0.0",
     "category": "Construction",
     "website": "https://www.google.com/",
-    "depends": ["sale_management", "account", "hr", "stock", "project", "purchase"],
+    "depends": ["sale_management", "account", "hr", "stock", "project", "purchase","website"],
     "data": [
         "security/ir.model.access.csv",
-        "wizard/construction_site_wizard_view.xml",
         "views/construction_site_view.xml",
         "views/construction_portal_templates.xml",
         "views/purchase_order_inherit_view.xml",
         "views/sale_order_inherit_view.xml",
         "views/project_task_inherit_view.xml",
+        "wizard/construction_site_wizard_view.xml",
         "report/construction_site_report.xml",
     ],
     "auto_install": False,
     "installable": True,
     "application": True,
 }
+
