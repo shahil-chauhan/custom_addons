0c7af7a3a27eac7d4691e72468cbf53ef68f3a96
Shahil <shahil@icreativetechnolabs.com>
2021-12-02 18:23:46 +0530

    [UPDATED] Mail completed

--- credit_block_limit/models/sale_order.py
+++ credit_block_limit/models/sale_order.py
@@ -13,38 +13,31 @@ class SaleOrder(models.Model):
         blocking_limit = sale_order.partner_id.blocking_limit
         credit_limit_score = sale_order.partner_id.credit_limit_score
         blocking_limit_score = sale_order.partner_id.blocking_limit_score
-        email_to = (
-            self.env["ir.config_parameter"]
-            .sudo()
-            .get_param("credit_block_limit.notify_person")
-        )
-        print("\n\nemail", email_to)
+        amount_total = sale_order.amount_total
 
+        email_to = (self.env["ir.config_parameter"].sudo().get_param("credit_block_limit.notify_person"))
         # to check for the credit limit
         if credit_limit_score and credit_limit:
-            total = sale_order.amount_total
             for order in self.env["sale.order"].search(
-                [("partner_id", "=", sale_order.partner_id.id), ("state", "=", "draft")]
-            ):
-                total += order.amount_total
-            if total > credit_limit_score:
+                    [("partner_id", "=", sale_order.partner_id.id), ("state", "=", "draft")]):
+                amount_total += order.amount_total
+            if amount_total > credit_limit_score:
                 if email_to:
                     template = sale_order.env.ref(
                         "credit_block_limit.limit_mail_template"
                     )
-                    print("\n\nemailtemplate ", template)
+                    template.send_mail(sale_order.id, email_values={"email_to": email_to}, force_send=True)
 
-                    temp = template.send_mail(sale_order.id, force_send=True)
-                    print("\n\nemail sent ", temp)
+                    super(SaleOrder, self).unlink()  # to unlink the record
+                    self._cr.commit()
                 raise ValidationError(
                     _('“Your Sale Order Credit Limit has been Exceeded."')
                 )
 
         # to check for the blocking limit
         if blocking_limit_score and blocking_limit:
-            amount_total = sale_order.amount_total
             for order in self.env["sale.order"].search(
-                [("partner_id", "=", sale_order.partner_id.id), ("state", "=", "sale")]
+                    [("partner_id", "=", sale_order.partner_id.id), ("state", "=", "sale")]
             ):
                 amount_total += order.amount_total
             if amount_total > blocking_limit_score:
@@ -55,6 +48,9 @@ class SaleOrder(models.Model):
                     template.send_mail(
                         sale_order.id, email_values={"email_to": email_to}
                     )
+                    salesuper(SaleOrder, self).unlink()  # to unlink the record
+                    self._cr.commit()
+
                 raise ValidationError(
                     _("“You cannot create SO as the Block Limit has been Exceeded”")
                 )
