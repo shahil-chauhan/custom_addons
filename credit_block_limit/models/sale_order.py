aed40f39ed613b328d66decd59b758807fe2e9b5
Shahil <shahil@icreativetechnolabs.com>
2021-12-03 14:37:20 +0530

    [DONE] Mail template

--- credit_block_limit/models/sale_order.py
+++ credit_block_limit/models/sale_order.py
@@ -14,21 +14,27 @@ class SaleOrder(models.Model):
         credit_limit_score = sale_order.partner_id.credit_limit_score
         blocking_limit_score = sale_order.partner_id.blocking_limit_score
         amount_total = sale_order.amount_total
-
+        partner = sale_order["partner_id"]
         email_to = (self.env["ir.config_parameter"].sudo().get_param("credit_block_limit.notify_person"))
+
         # to check for the credit limit
         if credit_limit_score and credit_limit:
             for order in self.env["sale.order"].search(
                     [("partner_id", "=", sale_order.partner_id.id), ("state", "=", "draft")]):
                 amount_total += order.amount_total
+
             if amount_total > credit_limit_score:
                 if email_to:
-                    template = sale_order.env.ref(
-                        "credit_block_limit.limit_mail_template"
-                    )
-                    template.send_mail(sale_order.id, email_values={"email_to": email_to}, force_send=True)
-
-                    super(SaleOrder, self).unlink()  # to unlink the record
+                    print("\n\n\n email_to", email_to)
+                    context = {
+                        "exceeded_limit": "Credit",
+                        "exceeding_limit_amount": credit_limit_score
+                    }
+                    template = self.env.ref("credit_block_limit.limit_mail_template")
+                    print("\n\n\n template", template)
+                    template.with_context(context).send_mail(partner.id, email_values={"email_to": email_to},
+                                                             force_send=True)
+                    super(SaleOrder, sale_order).unlink()
                     self._cr.commit()
                 raise ValidationError(
                     _('“Your Sale Order Credit Limit has been Exceeded."')
@@ -40,18 +46,19 @@ class SaleOrder(models.Model):
                     [("partner_id", "=", sale_order.partner_id.id), ("state", "=", "sale")]
             ):
                 amount_total += order.amount_total
+
             if amount_total > blocking_limit_score:
                 if email_to:
-                    template = sale_order.env.ref(
-                        "credit_block_limit.limit_mail_template"
-                    )
-                    template.send_mail(
-                        sale_order.id, email_values={"email_to": email_to}
-                    )
-                    salesuper(SaleOrder, self).unlink()  # to unlink the record
+                    context = {
+                        "exceeded_limit": "Block",
+                        "exceeding_limit_amount": blocking_limit_score,
+                    }
+                    template = sale_order.env.ref("credit_block_limit.limit_mail_template")
+                    template.with_context(context).send_mail(partner.id, email_values={"email_to": email_to},
+                                                             force_send=True)
+                    super(SaleOrder, sale_order).unlink()
                     self._cr.commit()
-
                 raise ValidationError(
-                    _("“You cannot create SO as the Block Limit has been Exceeded”")
+                    _("“You cannot create SO as the Block Limit has been Exceeded.”")
                 )
         return sale_order
